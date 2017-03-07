a28f8622bc624320bb6134c3612e540c430df06a
Sambhav Jain <sambhav.jiit@gmail.com>
2017-03-07 18:06:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var app = express();
-
 var port = process.env.PORT || 8080;
-
-
-
 require('./routes.js')(app);
-
 app.listen(8080, function(){
 	console.log("server listening on port: "+port)
 })
