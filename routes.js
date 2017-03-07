8e7fc1350fb76918124f254c74b2d95a115ff180
Sambhav Jain <sambhav.jiit@gmail.com>
2017-03-07 18:07:25 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 var Rx = require('./rx'),
-	Bluebird = require('./bluebird')
-
+var Bluebird = require('./bluebird')
 module.exports = function(app){
 	app.get('/api_for_rx', Rx.fetch_rx_data)
 	app.get('/api_for_bluebird', Bluebird.fetch_bluebird_data)
